d5c03c91d868fc57d0d6733ad5445cee48541ba0
bokwangmusic <bokwangmusic@gmail.com>
2026-09-15 23:58:17 +0900

    feat: initialize SQLite at app root

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,11 @@
 import { Stack } from 'expo-router';
+import { SQLiteProvider } from 'expo-sqlite';
 import { StatusBar } from 'expo-status-bar';
+import { DATABASE_NAME, migrateDatabase } from '../src/db/database';
 
 export default function RootLayout() {
   return (
-    <>
+    <SQLiteProvider databaseName={DATABASE_NAME} onInit={migrateDatabase}>
       <StatusBar style="dark" />
       <Stack
         screenOptions={{
@@ -11,6 +13,6 @@ export default function RootLayout() {
           contentStyle: { backgroundColor: '#F7F8FA' },
         }}
       />
-    </>
+    </SQLiteProvider>
   );
 }
