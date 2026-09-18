15556e97805ef7a46e8a5c9e0931cf2b37247d3d
bokwangmusic <bokwangmusic@gmail.com>
2026-09-18 16:44:09 +0900

    feat: render multiple schedule widget sizes

--- src/widgets/widgetTaskHandler.tsx
+++ src/widgets/widgetTaskHandler.tsx
@@ -2,25 +2,64 @@
 
 import React from 'react';
 import type { WidgetTaskHandlerProps } from 'react-native-android-widget';
+import { TodayScheduleWidget } from './TodayScheduleWidget';
 import { WeeklyTimetableWidget } from './WeeklyTimetableWidget';
 import { loadWeeklyWidgetData } from './widgetData';
 
-export async function widgetTaskHandler(props: WidgetTaskHandlerProps) {
-  if (props.widgetInfo.widgetName !== 'WeeklyTimetable') return;
+function renderScheduleWidget(
+  widgetName: string,
+  data: Awaited<ReturnType<typeof loadWeeklyWidgetData>>,
+  width: number,
+  height: number,
+) {
+  switch (widgetName) {
+    case 'TodaySchedule':
+      return <TodayScheduleWidget data={data} widgetHeight={height} />;
 
+    case 'WeeklyTimetableCompact':
+      return (
+        <WeeklyTimetableWidget
+          data={data}
+          widgetWidth={width}
+          widgetHeight={height}
+          variant="compact"
+        />
+      );
+
+    case 'WeeklyTimetable':
+      return (
+        <WeeklyTimetableWidget
+          data={data}
+          widgetWidth={width}
+          widgetHeight={height}
+          variant="large"
+        />
+      );
+
+    default:
+      return null;
+  }
+}
+
+export async function widgetTaskHandler(props: WidgetTaskHandlerProps) {
   switch (props.widgetAction) {
     case 'WIDGET_ADDED':
     case 'WIDGET_UPDATE':
     case 'WIDGET_RESIZED': {
       const data = await loadWeeklyWidgetData();
-      props.renderWidget(
-        <WeeklyTimetableWidget
-          data={data}
-          widgetHeight={props.widgetInfo.height}
-        />,
+      const widget = renderScheduleWidget(
+        props.widgetInfo.widgetName,
+        data,
+        props.widgetInfo.width,
+        props.widgetInfo.height,
       );
+
+      if (widget) {
+        props.renderWidget(widget);
+      }
       break;
     }
+
     case 'WIDGET_DELETED':
     case 'WIDGET_CLICK':
     default:
