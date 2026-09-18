763844d6d4aa2edb88375f05bf0a6682c11e0324
bokwangmusic <bokwangmusic@gmail.com>
2026-09-18 16:44:20 +0900

    feat: refresh and pin all schedule widgets

--- src/widgets/widgetController.tsx
+++ src/widgets/widgetController.tsx
@@ -5,23 +5,56 @@ import {
   requestPinWidget,
   requestWidgetUpdate,
 } from 'react-native-android-widget';
+import { TodayScheduleWidget } from './TodayScheduleWidget';
 import { WeeklyTimetableWidget } from './WeeklyTimetableWidget';
 import { loadWeeklyWidgetData } from './widgetData';
 
 export async function refreshWeeklyTimetableWidget() {
   const data = await loadWeeklyWidgetData();
 
-  await requestWidgetUpdate({
-    widgetName: 'WeeklyTimetable',
-    renderWidget: (widgetInfo) => (
-      <WeeklyTimetableWidget
-        data={data}
-        widgetHeight={widgetInfo.height}
-      />
-    ),
-  });
+  await Promise.all([
+    requestWidgetUpdate({
+      widgetName: 'WeeklyTimetable',
+      renderWidget: (widgetInfo) => (
+        <WeeklyTimetableWidget
+          data={data}
+          widgetWidth={widgetInfo.width}
+          widgetHeight={widgetInfo.height}
+          variant="large"
+        />
+      ),
+    }),
+    requestWidgetUpdate({
+      widgetName: 'WeeklyTimetableCompact',
+      renderWidget: (widgetInfo) => (
+        <WeeklyTimetableWidget
+          data={data}
+          widgetWidth={widgetInfo.width}
+          widgetHeight={widgetInfo.height}
+          variant="compact"
+        />
+      ),
+    }),
+    requestWidgetUpdate({
+      widgetName: 'TodaySchedule',
+      renderWidget: (widgetInfo) => (
+        <TodayScheduleWidget
+          data={data}
+          widgetHeight={widgetInfo.height}
+        />
+      ),
+    }),
+  ]);
 }
 
 export async function pinWeeklyTimetableWidget() {
   return requestPinWidget({ widgetName: 'WeeklyTimetable' });
+}
+
+export async function pinWeeklyTimetableCompactWidget() {
+  return requestPinWidget({ widgetName: 'WeeklyTimetableCompact' });
+}
+
+export async function pinTodayScheduleWidget() {
+  return requestPinWidget({ widgetName: 'TodaySchedule' });
 }
